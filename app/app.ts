50cebcf5e799f571daaa501ca506f166e682cea8
Kunio Sawatani <devel@fathens.org>
2016-09-06 15:42:16 +0900

    app.[ts, html] を作成

--- app/app.ts
+++ app/app.ts
@@ -1,23 +1,43 @@
-import { Component } from '@angular/core';
-import { ionicBootstrap, Platform } from 'ionic-angular';
-import { StatusBar } from 'ionic-native';
+import { Component, ViewChild } from '@angular/core';
+import { App, ionicBootstrap, Platform, Nav } from 'ionic-angular';
+import { AppVersion, StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from './pages/home/home';
-
+import { withFabric } from "./util/fabric";
+import { Logger } from "./util/logging";
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+    templateUrl: "build/app.html"
 })
 export class MyApp {
-  rootPage: any = HomePage;
+    @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-    });
-  }
+    rootPage: any = HomePage;
+
+    isDevel: boolean = false;
+
+    constructor(private app: App, platform: Platform) {
+        platform.ready().then(async () => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            StatusBar.styleDefault();
+            Splashscreen.hide();
+            await Logger.setLebelByVersionNumber();
+            try {
+                const version: string = await AppVersion.getVersionNumber();
+                const v = parseInt(_.last(version.match(/[0-9]/g)));
+                this.isDevel = v % 2 !== 0;
+            } catch (ex) { }
+        });
+    }
+
+    crash() {
+        withFabric((fabric) => fabric.Crashlytics.crash("Manually crashed."));
+    }
+
+    openPage(page) {
+        this.nav.setRoot(page.component);
+    }
 }
 
 ionicBootstrap(MyApp);
