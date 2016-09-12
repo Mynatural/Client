50067bca0e6ee19a39d6347befb07d212fd4829a
Kunio Sawatani <devel@fathens.org>
2016-09-12 14:29:50 +0900

    slides で商品ラインナップを表示

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
+
+import {FATHENS_DIRECTIVES} from "../../components/all";
+import {FATHENS_PROVIDERS} from "../../providers/all";
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+    templateUrl: 'build/pages/home/home.html',
+    directives: [FATHENS_DIRECTIVES],
+    providers: [FATHENS_PROVIDERS]
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) { }
 
-  }
-
-  static async create(name: String): Promise<String> {
-      return _.isNil(name) ? "" : name;
-  }
+    items: Array<Item> = [
+        { key: "short-sleeved", name: "半袖", image: "unauthorized/images/short-sleeved.jpg" },
+        { key: "long-sleeved", name: "長袖", image: "unauthorized/images/long-sleeved.jpg" },
+        { key: "no-sleeved", name: "袖無し", image: "unauthorized/images/no-sleeved.jpg" },
+    ];
+    slideOptions = {
+        loop: true,
+        pager: true,
+        autoplay: 3000,
+        speed: 700
+    };
 }
+
+type Item = {
+    key: string,
+    name: string,
+    image: string
+};
