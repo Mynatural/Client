ddcd3b569257964ec1c857efd47d2898f2afe2ca
Kunio Sawatani <devel@fathens.org>
2016-09-12 14:25:54 +0900

    providers を追加

---
+++ app/providers/all.ts
@@ -0,0 +1,19 @@
+import {BootSettings} from "./config/boot_settings";
+import {Configuration} from "./config/configuration";
+import {Preferences} from "./config/preferences";
+import {S3File} from "./aws/s3file";
+import {Cognito} from "./aws/cognito";
+import {Dynamo} from "./aws/dynamo/dynamo";
+import {FBConnect} from "./facebook/fb_connect";
+import {FBJSSDK} from "./facebook/fb_jssdk";
+
+export const FATHENS_PROVIDERS = [
+    BootSettings,
+    Configuration,
+    Preferences,
+    S3File,
+    Cognito,
+    Dynamo,
+    FBConnect,
+    FBJSSDK
+];
---
+++ app/providers/facebook/fb_jssdk.ts
@@ -0,0 +1,101 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+
+import {toPromise} from "../../util/promising";
+import {Logger} from "../../util/logging";
+
+const logger = new Logger("FBJSSDK");
+
+@Injectable()
+export class FBJSSDK implements FBConnectPlugin {
+    constructor(private http: Http) { }
+
+    private async initialize(): Promise<void> {
+        const scriptId = "facebook-jssdk";
+        if (!_.isNil(document.getElementById(scriptId))) return;
+
+        const appId = (await toPromise(this.http.get("facebook_app_id"))).text().trim();
+        logger.debug(() => `Setting browser facebook app id: ${appId}`);
+
+        const script = document.createElement("script") as HTMLScriptElement;
+        script.id = scriptId;
+        script.src = "https://connect.facebook.net/en_US/sdk.js";
+
+        return new Promise<void>((resolve, reject) => {
+            (window as any).fbAsyncInit = () => {
+                try {
+                    (window as any).FB.init({
+                        appId: appId,
+                        xfbml: false,
+                        version: "v2.5"
+                    });
+                    resolve();
+                } catch (ex) {
+                    reject(ex);
+                }
+            };
+            const first = document.getElementsByTagName("script")[0];
+            first.parentNode.insertBefore(script, first);
+        });
+    }
+
+    private async invoke<T>(proc: (fb: FBJSSDKPlugin, callback: (result: T) => void) => void) {
+        await this.initialize();
+        return new Promise<T>((resolve, reject) => {
+            proc((window as any).FB, resolve);
+        });
+    }
+
+    login(arg?: string): Promise<string> {
+        return this.invoke<string>((fb, callback) => {
+            const args = ["public_profile"];
+            if (arg) args.push(arg);
+            fb.login((res) => {
+                callback(res.authResponse.accessToken);
+            }, { scope: args.join(",") });
+        });
+    }
+
+    logout(): Promise<void> {
+        return this.invoke<void>((fb, callback) => {
+            fb.logout(callback);
+        });
+    }
+
+    getName(): Promise<string> {
+        throw "Unsupported oparation: getName";
+    }
+
+    getToken(): Promise<FBConnectToken> {
+        return this.invoke<FBConnectToken>((fb, callback) => {
+            fb.getLoginStatus((res) => {
+                if (res.status === "connected") {
+                    callback({
+                        token: res.authResponse.accessToken,
+                        permissions: null
+                    });
+                } else {
+                    callback(null);
+                }
+            });
+        });
+    }
+}
+
+declare type FBJSCallback<T> = (res: T) => void;
+
+interface FBJSSDKPlugin {
+    login(callback: FBJSCallback<LoginResponse>, param): void;
+    logout(callback: FBJSCallback<void>): void;
+    getLoginStatus(callback: FBJSCallback<LoginResponse>): void;
+}
+
+interface LoginResponse {
+    authResponse: {
+        accessToken: string,
+        userID: string,
+        expiresIn: number,
+        signedRequest: string
+    };
+    status: string;
+}
