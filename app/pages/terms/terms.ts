230fbd0fe3da26901cbe265f2386c2ce288186fa
Kunio Sawatani <devel@fathens.org>
2016-09-16 14:01:57 +0900

    利用規約を Gist で書く

--- app/pages/terms/terms.ts
+++ app/pages/terms/terms.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import {FATHENS_DIRECTIVES} from "../../components/all";
+import {FATHENS_PROVIDERS} from "../../providers/all";
+
 @Component({
-  templateUrl: 'build/pages/terms/terms.html'
+    templateUrl: 'build/pages/terms/terms.html',
+    directives: [FATHENS_DIRECTIVES],
+    providers: [FATHENS_PROVIDERS]
 })
 export class TermsPage {
     static title = "利用規約";
     static icon = "contract";
     title = TermsPage.title;
-
-    constructor(public navCtrl: NavController) {
-
-    }
-
-    static async create(name: String): Promise<String> {
-        return _.isNil(name) ? "" : name;
-    }
 }
