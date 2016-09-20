b03a473d3ab5d5a5816f7559ee03da287e3b6663
Kunio Sawatani <devel@fathens.org>
2016-09-20 14:56:21 +0900

    Custom ページの雛形を追加

---
+++ app/pages/custom/custom.ts
@@ -0,0 +1,21 @@
+import {Component} from "@angular/core";
+import {SafeUrl} from '@angular/platform-browser';
+import {NavController, NavParams} from "ionic-angular";
+
+import {S3Image} from "../../providers/aws/s3file";
+import {Logger} from "../../util/logging";
+
+const logger = new Logger("CustomPage");
+
+@Component({
+    templateUrl: 'build/pages/custom/custom.html'
+})
+export class CustomPage {
+    title: string;
+    itemKey: string;
+
+    constructor(private params: NavParams) {
+        this.itemKey = params.get('key');
+        this.title = params.get('name');
+    }
+}
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -2,7 +2,11 @@ import {Component} from "@angular/core";
 import {SafeUrl} from '@angular/platform-browser';
 import {NavController} from "ionic-angular";
 
+import {CustomPage} from "../custom/custom";
 import {S3Image} from "../../providers/aws/s3file";
+import {Logger} from "../../util/logging";
+
+const logger = new Logger("HomePage");
 
 const lineup = [
     { key: "short-sleeved", name: "はんそで" },
@@ -30,7 +34,7 @@ export class HomePage {
         "で作っちゃおう！"
     ];
 
-    constructor(public navCtrl: NavController, s3image: S3Image) {
+    constructor(public nav: NavController, s3image: S3Image) {
         Promise.all(lineup.map(async (item) => {
             return {
                 key: item.key,
@@ -53,4 +57,9 @@ export class HomePage {
         autoplay: 3000,
         speed: 700
     };
+
+    choose(item: Item) {
+        logger.info(() => `Choose ${item.key}`);
+        this.nav.push(CustomPage, item);
+    }
 }
