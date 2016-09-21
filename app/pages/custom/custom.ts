ab7448d08058b8d163a0728a247d55b7935e94e1
Kunio Sawatani <devel@fathens.org>
2016-09-21 18:10:59 +0900

    Custom ページのモックを作成

--- app/pages/custom/custom.ts
+++ app/pages/custom/custom.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 import {SafeUrl} from '@angular/platform-browser';
 import {NavController, NavParams} from "ionic-angular";
 
-import {S3Image} from "../../providers/aws/s3file";
+import {Lineups, Lineup} from "../../providers/model/lineup";
 import {Logger} from "../../util/logging";
 
 const logger = new Logger("CustomPage");
@@ -13,13 +13,40 @@ const logger = new Logger("CustomPage");
 export class CustomPage {
     title: string;
     itemKey: string;
+    item: Lineup;
 
-    constructor(private params: NavParams) {
+    onFront: boolean = true;
+
+    priceMessage = "現在のお値段";
+    priceUnit = "￥";
+    price = 1234;
+
+    constructor(private params: NavParams, private lineups: Lineups) {
         this.itemKey = params.get('key');
         this.title = params.get('name');
+        lineups.get(this.itemKey).then((item) => {
+            this.item = item;
+        });
+    }
+
+    get isReady(): boolean {
+        return this.item != null;
     }
 
     turn_over() {
         logger.debug(() => `TurnOver`);
     }
+
+    specs = [
+        {
+            class: "bold",
+            name: "胴まわり",
+            value: "70cm"
+        },
+        {
+            class: "sleeve",
+            name: "そでの長さ",
+            value: "20cm"
+        }
+    ];
 }
