248fb06f45a2dd75fb6a6c8bbf496568d07cc5cf
Kunio Sawatani <devel@fathens.org>
2016-09-23 13:28:48 +0900

    turnOver のアニメーションを追加

--- app/pages/custom/custom.ts
+++ app/pages/custom/custom.ts
@@ -1,4 +1,10 @@
-import {Component} from "@angular/core";
+import {Component,
+  trigger,
+  state,
+  style,
+  transition,
+  keyframes,
+  animate} from "@angular/core";
 import {SafeUrl} from '@angular/platform-browser';
 import {NavController, NavParams} from "ionic-angular";
 
@@ -7,15 +13,38 @@ import {Logger} from "../../util/logging";
 
 const logger = new Logger("CustomPage");
 
+const stateOn = "active";
+const stateOff = "inactive";
+
 @Component({
-    templateUrl: 'build/pages/custom/custom.html'
+    templateUrl: 'build/pages/custom/custom.html',
+    animations: [
+        trigger("turnOver", [
+            state(stateOn, style({display: "flex"})),
+            state(stateOff, style({display: "none"})),
+            transition(`${stateOn} => ${stateOff}`, [
+                animate("0.5s 0s linear", keyframes([
+                    style({transform: "rotateY(0)"}),
+                    style({transform: "rotateY(90deg)"}),
+                    style({transform: "rotateY(90deg)"})
+                ]))
+            ]),
+            transition(`${stateOff} => ${stateOn}`, [
+                animate("0.5s 0s linear", keyframes([
+                    style({transform: "rotateY(-90deg)"}),
+                    style({transform: "rotateY(-90deg)"}),
+                    style({transform: "rotateY(0)"})
+                ]))
+            ])
+        ])
+    ]
 })
 export class CustomPage {
     title: string;
     itemKey: string;
     item: Lineup;
 
-    onFront: boolean = true;
+    isFront = true;
 
     priceMessage = "現在のお値段";
     priceUnit = "￥";
@@ -33,8 +62,16 @@ export class CustomPage {
         return this.item != null;
     }
 
+    get onFront(): string {
+        return this.isFront ? stateOn : stateOff;
+    }
+
+    get onBack(): string {
+        return !this.isFront ? stateOn : stateOff;
+    }
+
     turn_over() {
-        logger.debug(() => `TurnOver`);
+        this.isFront = !this.isFront;
     }
 
     specs = [
