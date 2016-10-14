6337366f8b8d56784f3f3c2c0ab1e2ea0ab4036b
Kunio SAWATANI <fsawa@fathens.org>
2016-10-14 09:36:12 +0000

    babel-polyfill をインポート

--- src/app/main.dev.ts
+++ src/app/main.dev.ts
@@ -1,5 +1,10 @@
+import _babel from "babel-core/register";
+import _polyfill from "babel-polyfill";
+
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "./app.module";
+
+console.log(`babel-core=${_babel}, polyfill=${_polyfill}`);
 
 platformBrowserDynamic().bootstrapModule(AppModule);
