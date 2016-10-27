d3d4961a2dfbcf82ddf0a959b96f0e6e4e3a0317
Kunio SAWATANI <fsawa@fathens.org>
2016-10-27 02:19:19 +0000

    BootSettings のソースを Immutable に変更
    Immutable Map を使うのと、Load が２重に走ってしまう問題を修正しました。

--- src/providers/config/boot_settings.ts
+++ src/providers/config/boot_settings.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
+import Im from "immutable";
 
 import { toPromise } from "../util/promising";
 import { Logger } from "../util/logging";
@@ -8,17 +9,21 @@ const logger = new Logger("BootSettings");
 
 @Injectable()
 export class BootSettings {
-    private static src: { [key: string]: string; } = null;
+    private static src: Promise<Im.Map<string, string>>;
 
     constructor(private http: Http) { }
 
+    private async load(): Promise<Im.Map<string, string>> {
+        logger.debug(() => `Loading settings.json...`);
+        const res = await toPromise(this.http.get("settings.json"));
+        return Im.Map<string, string>(JSON.parse(res.text()));
+    }
+
     private async get(key: string): Promise<string> {
         if (_.isNil(BootSettings.src)) {
-            logger.debug(() => `Loading settings.json...`);
-            const res = await toPromise(this.http.get("settings.json"));
-            BootSettings.src = JSON.parse(res.text());
+            BootSettings.src = this.load();
         }
-        return BootSettings.src[key];
+        return (await BootSettings.src).get(key);
     }
 
     get awsRegion(): Promise<string> {
