b02d91eb9f99df2956ddb0d1c08b1579bb91ba86
Kunio SAWATANI <fsawa@fathens.org>
2016-11-04 11:31:25 +0000

    news と categories の読み書きを実装

--- src/providers/model/lineup/category.ts
+++ src/providers/model/lineup/category.ts
@@ -10,32 +10,55 @@ import * as Base64 from "../../util/base64";
 
 const logger = new Logger("Lineup.Category");
 
+const newsJson = "news.json";
+const categoriesJson = "categories.json";
+
+function pathJson(key: string): string {
+    return _.join([ROOT, LINEUP, key], "/");
+}
+
 export class Category {
     private static async load(s3file: S3File, key: string): Promise<any> {
-        const path = _.join([ROOT, LINEUP, key], "/");
+        const path = pathJson(key);
         const text = await s3file.read(path);
         return Base64.decodeJson(text);
     }
 
-    static async news(s3file: S3File, srcList: Im.List<Item>): Promise<Category> {
-        const v = (await Category.load(s3file, "news.json")) as Info.Category;
-        return new Category("news", srcList, v);
+    private static async save(s3file: S3File, key: string, obj: any): Promise<void> {
+        const path = pathJson(key);
+        const text = Base64.encodeJson(obj);
+        await s3file.write(path, text);
     }
 
-    static async byAll(s3file: S3File, srcList: Im.List<Item>): Promise<Im.List<Category>> {
-        const json = (await Category.load(s3file, "categories.json")) as Info.Categories;
-        const array = _.map(json, (v, key) => {
-            return new Category(key, srcList, v);
-        });
-        return Im.List(array);
+    static async loadNews(s3file: S3File, srcList: Im.List<Item> = Im.List<Item>()): Promise<Category> {
+        const v = (await Category.load(s3file, newsJson)) as Info.Category;
+        return new Category(v, srcList);
     }
 
-    constructor(
-        public readonly key: string,
-        public readonly srcList: Im.List<Item>,
-        private json: Info.Category
-    ) {
+    static async loadAll(s3file: S3File, srcList: Im.List<Item> = Im.List<Item>()): Promise<Im.Map<string, Category>> {
+        const json = (await Category.load(s3file, categoriesJson)) as Info.Categories;
+        const map = _.mapValues(json, (v) => new Category(v, srcList));
+        return Im.Map(map);
+    }
+
+    static async saveNews(s3file: S3File, obj: Info.Category): Promise<void> {
+        await Category.save(s3file, newsJson, obj);
+    }
+
+    static async saveAll(s3file: S3File, obj: Info.Categories): Promise<void> {
+        await Category.save(s3file, categoriesJson, obj);
+    }
+
+    constructor(private json: Info.Category, public readonly srcList: Im.List<Item> = Im.List<Item>()) {
         this._flags = Im.Map(json.flags);
+    }
+
+    asJSON(): Info.Category {
+        return {
+            title: this.title,
+            message: this.message,
+            flags: this.flags.toObject()
+        }
     }
 
     private _flags: Im.Map<string, string>;
