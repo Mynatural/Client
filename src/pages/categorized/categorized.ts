a8cfe38b2291195d6740e9fadd833b8d05aa8d52
Kunio SAWATANI <fsawa@fathens.org>
2016-11-08 06:31:38 +0000

    CategoryPage から CustomPage へのジャンプを追加

--- src/pages/categorized/categorized.ts
+++ src/pages/categorized/categorized.ts
@@ -2,9 +2,11 @@ import _ from "lodash";
 import Im from "immutable";
 
 import { Component } from "@angular/core";
-import { NavParams } from "ionic-angular";
+import { NavController, NavParams } from "ionic-angular";
 
+import { CustomPage } from "../custom/custom";
 import { CategoryController, Category } from "../../providers/model/lineup/category";
+import { Item } from "../../providers/model/lineup/item";
 import { Logger } from "../../providers/util/logging";
 
 const logger = new Logger("CategorizedPage");
@@ -22,10 +24,17 @@ export class CategorizedPage {
     category: Category;
     categories: Category[];
 
-    constructor(params: NavParams, private ctgCtrl: CategoryController) {
+    constructor(params: NavParams, private nav: NavController, private ctgCtrl: CategoryController) {
         this.category = params.get("category");
         ctgCtrl.loadAll().then((v) => {
             this.categories = v.toArray();
+        });
+    }
+
+    choose(item: Item) {
+        logger.info(() => `Choose ${item.key}`);
+        this.nav.push(CustomPage, {
+            item: item
         });
     }
 }
