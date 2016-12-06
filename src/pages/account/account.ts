788675ac2fbf9f18f59f46f7ebc2caf503156f63
Kunio SAWATANI <fsawa@fathens.org>
2016-12-06 11:43:33 +0000

    AccountPage 内で SocialConnection を抽象化

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -1,8 +1,6 @@
 import _ from "lodash";
-import Im from "immutable";
 
 import { Component } from "@angular/core";
-import { NavController } from "ionic-angular";
 
 import { Cognito } from "../../providers/aws/cognito";
 import { Logger } from "../../providers/util/logging";
@@ -21,19 +19,67 @@ export class AccountPage {
     readonly titleConnections = "接続サービス";
     readonly titlePoints = "ポイント";
 
-    faceook: boolean = false;
-    instagram: boolean = false;
-    line: boolean = false;
+    readonly sns = [
+        new SocialConnection("Facebook", "FB-f-Logo__blue_72.png",
+            async () => (await this.cognito.identity).isJoinFacebook,
+            async () => await this.cognito.joinFacebook(),
+            async () => await this.cognito.dropFacebook()
+        ),
+        new SocialConnection("Instagram", "Instagram-v051916-72.png",
+            async () => false,
+            async () => {},
+            async () => {}
+        ),
+        new SocialConnection("LINE", "LINE_Icon-72.png",
+            async () => false,
+            async () => {},
+            async () => {}
+        )
+    ]
 
     constructor(private cognito: Cognito) {
     }
+}
 
-    async connectFacebook(): Promise<void> {
-        await this.cognito.joinFacebook();
+export class SocialConnection {
+    constructor(
+        public readonly name,
+        public readonly logo,
+        private isConnected: () => Promise<boolean>,
+        private connect: () => Promise<void>,
+        private disconnect: () => Promise<void>
+    ) {
+        logger.debug(() => `Creating SocialConnection: ${this.name}`);
     }
 
-    async disconnectFacebook(): Promise<void> {
-        await this.cognito.dropFacebook();
+    private _isConnected: boolean = false;
+    private _isConnectedWait: Promise<boolean>;
+
+    get toggle(): boolean {
+        if (_.isNil(this._isConnectedWait)) {
+            this._isConnectedWait = this.isConnected();
+            this._isConnectedWait.then((v) => {
+                this._isConnected = v;
+            })
+        }
+        return this._isConnected;
     }
 
+    set toggle(v: boolean) {
+        this.update(v);
+    }
+
+    private async update(doConnect: boolean) {
+        try {
+            if (doConnect) {
+                await this.connect();
+            } else {
+                await this.disconnect();
+            }
+        } catch (ex) {
+            logger.warn(() => `Failed to update social connection: ${ex}`);
+        } finally {
+            this._isConnected = await this.isConnected();
+        }
+    }
 }
